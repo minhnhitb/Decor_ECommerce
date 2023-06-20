55ec69536d519f1857497fa6ae98afc7e246479d
newstar94 <77623864+newstar94@users.noreply.github.com>
2023-06-20 15:25:08 +0700

    update payment, order

--- src/main/java/com/example/DecorEcomerceProject/Entities/DTO/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.DecorEcomerceProject.Entities.DTO;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String phone;
-    private String password;
-}
--- src/main/java/com/example/DecorEcomerceProject/Entities/DTO/RoleToUser.java
+++
@@ -1,9 +0,0 @@
-package com.example.DecorEcomerceProject.Entities.DTO;
-
-import lombok.Data;
-
-@Data
-public class RoleToUser {
-    private String phone;
-    private String roleName;
-}
